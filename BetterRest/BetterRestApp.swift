fde8e4dca0f48c717a78292eb586aa6e2132491d
Luke Inger <luke.inger@jcb-finance.co.uk>
2021-06-16 19:52:42 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Luke Inger on 16/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
